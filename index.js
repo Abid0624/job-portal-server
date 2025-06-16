41607bdc399792e4b6577ec397dcfaea3ccad407
Abid064 <abidkhan09989@gmail.com>
2025-06-16 23:35:32 +0600

    post a job and posted jobs apis are made

--- index.js
+++ index.js
@@ -28,7 +28,13 @@ async function run() {
       .collection("job_applications");
 
     app.get("/jobs", async (req, res) => {
-      const cursor = jobsCollection.find();
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+
+      const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -37,6 +43,12 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
       res.send(result);
     });
 
